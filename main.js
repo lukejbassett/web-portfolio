679daeb9bf9ba021d067551161f402ddc6d4d16e
lukejbassett <bassettlukej@gmail.com>
2025-02-02 16:18:21 +0000

    New portfolio website v1.1

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
+// CV download function
+function downloadCV() {
+  window.open("./rsrc/CV Luke Bassett (23-10-23).pdf");
+}
+
+// navigation hamburger for mobile screens
 const btn = document.querySelector(".hamburger");
-const menu = document.querySelector(".nav--main-nav--links");
+const menu = document.querySelector(".nav-links");
 btn.addEventListener("click", function () {
   btn.classList.toggle("is-active");
   menu.classList.toggle("is-active");
