b81949df07ec0a39df495b16129bf089529bdb59
lukejbassett <bassettlukej@gmail.com>
2025-02-03 21:22:01 +0000

    v1.2.1 complete footer, to top button, fixed hero

--- main.js
+++ main.js
@@ -10,3 +10,27 @@ btn.addEventListener("click", function () {
   btn.classList.toggle("is-active");
   menu.classList.toggle("is-active");
 });
+
+// to top button
+const topButton = document.querySelector(".toTopBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topButton.style.display = "block";
+    topButton.style.opacity = "1";
+  } else {
+    topButton.style.display = "none";
+    topButton.style.opacity = "0";
+  }
+}
+
+function toTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
